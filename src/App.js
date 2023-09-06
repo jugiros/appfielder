310997226e85257e034a8c6556f61b13eb007a8b
jugiros <31550758+jugiros@users.noreply.github.com>
2023-09-06 12:51:17 -0500

    feat: Actualización para utilizar el store de redux.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Home from "./views/Home";
 import './styles/global.css';
+import {Provider} from "react-redux";
+import store from "./store";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Provider store={store}>
+        <Home />
+      </Provider>
     </div>
   );
 }
