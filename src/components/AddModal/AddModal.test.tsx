861bfc9c3b5be9780a82a84fadef8be693bf7a53
jugiros <31550758+jugiros@users.noreply.github.com>
2023-09-06 09:21:05 -0500

    feat: Creación de directorios separados para los componentes.

--- src/components/AddModal/AddModal.test.tsx
+++ src/components/AddModal/AddModal.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import AddModal from './AddModal';
+import AddModal from "./AddModal";
 
 describe('AddModal', () => {
   test('renders AddModal component', () => {
